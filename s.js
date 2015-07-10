3f05339f3f6177bfead225803997e28a30af7f09
MCzeKrakan <mczekrakan@gmail.com>
2015-07-10 14:06:07 +0200

    Update and rename s.json to s.js

---
+++ s.js
@@ -0,0 +1,16 @@
+{
+  "autocomplete": [
+    {
+      "trigger": "!",
+      "suggestions": [
+        "!woot",
+        { "string": "!eta", "icon": "icon-current-dj-white" },
+        { "string": "!ban", "icon": "icon-ban", "role": 3 }
+      ]
+    },
+    {
+      "trigger": "%",
+      "suggestions": [ "%shrug", "%lol", "%tableflip" ]
+    }
+  ]
+}
